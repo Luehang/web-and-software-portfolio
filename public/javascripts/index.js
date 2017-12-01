2d5a6ce13f6a42c24b31f21b6732613c1e1596c5
Lue Hang <Lue_Hang@hotmail.com>
2017-11-30 22:57:03 -0600

    Minor fixes for responsive image sorting lay out.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,7 +11,7 @@ setTimeout(function() {
   content[0].style.height = "100%";
   mainPageContainer[0].style.visibility = "visible";
   mainPageContainer[0].style.height = "200px";
-}, 3000);
+}, 10);
 
 /********************************************
 main page js
