0cb8dc32c822fb9c90cfa573f3b1a389d2de66db
Lue Hang <Lue_Hang@hotmail.com>
2017-12-22 01:05:48 -0600

    Added gulp minify css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,92 @@
 "use strict";
 
 const gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-  imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-     babel = require('gulp-babel'),
-      sass = require('gulp-sass'),
     cssmin = require('gulp-cssmin'),
-sourcemaps = require('gulp-sourcemaps');
+    rename = require('gulp-rename'),
+  imagemin = require('gulp-imagemin');
 
-gulp.task('compileSass', function() {
-    gulp.src('src/styles/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('../../maps'))
-        .pipe(gulp.dest('public/stylesheets'));
-});
+//       sass = require('gulp-sass'),
+//     concat = require('gulp-concat'),
+//     uglify = require('gulp-uglify'),
 
-gulp.task('minifyCss', ['compileSass'], function() {
+//      babel = require('gulp-babel'),
+// sourcemaps = require('gulp-sourcemaps');
+
+// gulp.task('compileSass', function() {
+//     gulp.src('src/styles/style.scss')
+//         .pipe(sourcemaps.init())
+//         .pipe(sass())
+//         .pipe(sourcemaps.write('../../maps'))
+//         .pipe(gulp.dest('public/stylesheets'));
+// });
+
+gulp.task('minifyCss', function() {
     gulp.src('public/stylesheets/style.css')
         .pipe(cssmin())
+        .pipe(rename('style.min.css'))
         .pipe(gulp.dest('public/stylesheets'));
 });
 
-gulp.task('concatScripts', function() {
-    return gulp.src([
-        'src/js/main.js',
-        'src/js/scroll-animation.js'
-    ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('index.js'))
-    .pipe(sourcemaps.write('../../maps'))
-    .pipe(gulp.dest('src/js'));
-});
+// gulp.task('concatScripts', function() {
+//     return gulp.src([
+//         'src/js/main.js',
+//         'src/js/scroll-animation.js'
+//     ])
+//     .pipe(sourcemaps.init())
+//     .pipe(concat('index.js'))
+//     .pipe(sourcemaps.write('../../maps'))
+//     .pipe(gulp.dest('src/js'));
+// });
 
-gulp.task('compileReact', function() {
-    return gulp.src(['src/app.js', 'src/components/*.js'])
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['env', 'react']
-        }))
-        .pipe(rename('bundle.js'))
-        .pipe(sourcemaps.write('../../maps'))
-        .pipe(gulp.dest('public'));
-});
+// gulp.task('compileReact', function() {
+//     return gulp.src(['src/app.js', 'src/components/*.js'])
+//         .pipe(sourcemaps.init())
+//         .pipe(babel({
+//             presets: ['env', 'react']
+//         }))
+//         .pipe(rename('bundle.js'))
+//         .pipe(sourcemaps.write('../../maps'))
+//         .pipe(gulp.dest('public'));
+// });
 
-gulp.task('minifyScripts', ['concatScripts'], function() {
-    return gulp.src('src/js/index.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(uglify())
-        .pipe(rename('index.min.js'))
-        .pipe(gulp.dest('public/javascripts'));
-});
+// gulp.task('minifyScripts', ['concatScripts'], function() {
+//     return gulp.src('src/js/index.js')
+//         .pipe(babel({
+//             presets: ['env']
+//         }))
+//         .pipe(uglify())
+//         .pipe(rename('index.min.js'))
+//         .pipe(gulp.dest('public/javascripts'));
+// });
 
-gulp.task('minifyThree', function() {
-    return gulp.src('src/js/three-orbit-controls.js')
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename('three-orbit-controls.min.js'))
-        .pipe(sourcemaps.write('../../maps'))
-        .pipe(gulp.dest('src/js'));
-});
+// gulp.task('minifyThree', function() {
+//     return gulp.src('src/js/three-orbit-controls.js')
+//         .pipe(sourcemaps.init())
+//         .pipe(uglify())
+//         .pipe(rename('three-orbit-controls.min.js'))
+//         .pipe(sourcemaps.write('../../maps'))
+//         .pipe(gulp.dest('src/js'));
+// });
 
-gulp.task('concatMinify', ['minifyScripts', 'minifyThree'], function() {
-    return gulp.src([
-        'src/js/three-orbit-controls.min.js',
-        'public/javascripts/index.min.js'
-    ])
-    .pipe(concat('index.min.js'))
-    .pipe(gulp.dest('public/javascripts'));
-})
+// gulp.task('concatMinify', ['minifyScripts', 'minifyThree'], function() {
+//     return gulp.src([
+//         'src/js/three-orbit-controls.min.js',
+//         'public/javascripts/index.min.js'
+//     ])
+//     .pipe(concat('index.min.js'))
+//     .pipe(gulp.dest('public/javascripts'));
+// });
 
 gulp.task('imageMin', function() {
-    gulp.src(['src/img/calc.jpg', 
-        'src/img/camper_leaderboard.jpg',
-        'src/img/game_of_life.jpg',
-        'src/img/lue_hang.jpg',
-        'src/img/markdown.jpg',
-        'src/img/pomodoro.jpg',
-        'src/img/quote.jpg',
-        'src/img/recipe_box.jpg',
-        'src/img/riverside-pizza.jpg',
-        'src/img/simon.jpg',
-        'src/img/tic_tac_toe.jpg',
-        'src/img/twitch.jpg',
-        'src/img/weather.jpg',
-        'src/img/wiki.jpg',])
+    gulp.src(['src/img/*.jpg', 
+        'src/img/*.png', 
+        'src/img/*.svg'])
             .pipe(imagemin({optimizationLevel: 5}))
             .pipe(gulp.dest('public/img'));
 });
 
-gulp.task('build', ['compileSass', 'concatMinify']);
+// gulp.task('build', ['compileSass', 'concatMinify']);
 
-gulp.task('production' ['minifyCss', 'concatMinify']);
+// gulp.task('production' ['minifyCss', 'concatMinify']);
 
-gulp.task('test', ['']);
+// gulp.task('default', ['']);
